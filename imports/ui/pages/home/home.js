b976ddfc20030a58f0661b88a4ea3ae070e24858
Mickaël Mathieu <mickael.mathieu@e-teach.ch>
2023-05-08 11:28:38 +0200

    display customer top 10 attacks

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -94,9 +94,50 @@ const makeTopAttacksChart = () => {
   new Chart(document.querySelector('#chartTopAttacks'), config);
 };
 
+const makeHostsChart = () => {
+  const hosts = [];
+
+  _.each(Audit.find({}).fetch(), (a) => {
+    if (/^[.-_a-zA-Z]+$/.test(a.host) && !/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/.test(a.host)) {
+      hosts.push(a.host);
+    }
+  });
+
+  const countAttacks = _.countBy(hosts);
+  const pairs = _.pairs(countAttacks);
+  const top10 = _.first(_.sortBy(pairs, (z) => z[1]).reverse(), 10);
+  const labels = _.map(top10, (z) => z[0]);
+  const data = _.map(top10, (z) => z[1]);
+  const backgroundColor = _.map(data, () => randHexColor());
+
+  const config = {
+    type: 'bar',
+    data: {
+      labels,
+      datasets: [
+        {
+          backgroundColor,
+          data,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Top 10 Hosts Attacks',
+      },
+    },
+  };
+
+  new Chart(document.querySelector('#chartTopHost'), config);
+};
+
 const makeChart = () => {
   makeSeverityChart();
   makeTopAttacksChart();
+  makeHostsChart();
 };
 
 Template.home.onCreated(function ConfigOnCreated() {
